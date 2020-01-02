49e73bb219709b13d438dfcd897fc1d5bd2be919
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2020-01-02 18:00:51 -0500

    returned css

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
-// import "./Home.css";
+import "./Home.css";
 import { Container, Row, Col } from 'reactstrap';
 import grants from "../../images/image2.jpg";
 // import grantsMobile from "../../images/image3.jpg";
 import bw from "../../images/bw2.jpg";
 import bwMobile from "../../images/bwmobile2.jpg";
 
-
 const Home = () => {
-
+  
   const imageUrl = useWindowWidth() >= 650 ? bw : bwMobile;
 
   return (
